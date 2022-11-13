9aec2c99766bd5de17bc92544332332da96e7196
szf01 <2176529058@qq.com>
2022-11-13 23:11:01 +0800

    画ref之前

--- src/mavros/mavros/src/SpeedControlSet_pub.cpp
+++ src/mavros/mavros/src/SpeedControlSet_pub.cpp
@@ -19,33 +19,34 @@
 #include<geometry_msgs/Twist.h>
 #include<iostream>
 
-#define PI 3.14159265
-#define P_speed_y 0.001
-#define P_pose_y 2
-#define P_speed_x 0.001
-#define P_pose_x 0.001
+#define PI 3.141592653
+#define P_speed_y -0
+#define P_pose_y -0
+#define P_speed_x -0
+#define P_pose_x -0
 
 ros::Publisher path_pub;
 ros::Publisher path_ref_pub; 
 ros::Publisher send_publisher;
+ros::Publisher ref_publisher;
 ros::Subscriber xbox_sub;
 nav_msgs::Path path_real;
 nav_msgs::Path path_reference;
 float speed[3];
 float pose[3];
-float pose_cal[3];
-int safe = 0;
+double pose_ref[3];
+bool safe = true;
 
 
 void xboxCallback(const geometry_msgs::Twist::ConstPtr& xbox)
 {
-    if(xbox->angular.z != 0)
+    if(xbox->angular.z != 0 || speed[0] > 1 || speed[1] > 1 || speed[2] > 1)
     {
-        safe = 1;
+        safe = false;
     }
     else if(xbox->angular.z == 0)
     {
-        safe = 0;
+        safe = true;
     }
 }
 
@@ -68,8 +69,8 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& odom)
     path_real.header.stamp = ros::Time::now();
     path_real.header.frame_id = "camera_odom_frame";
     path_pub.publish(path_real);
-    ROS_INFO("11111");
-    // printf("path_pub");
+    ROS_INFO("path_real publish successfully");
+    // printf("path_pub");+
     // printf("odom %.3lf %.3lf %.3lf\n", odom->pose.pose.position.x, odom->pose.pose.position.y, odom->pose.pose.position.z);
     //获取当前位姿进行
     pose[0] = odom -> pose.pose.position.x;
@@ -95,9 +96,9 @@ void ShowPath_ref()
     double x = 0.0;
     double y = 0.0;
     double th = 0.0;
-    double vx = 0.0520;
-    double vy = 0.0260;
-    double vth = 0.2;
+    double vx = 0.25;//真实速度0.25m/s
+    double vy = 0.2;//最大速度0.2m/s
+    double vth = 0.5;
     ros::Rate loop_rate(50);
     while (ros::ok())
     {
@@ -106,14 +107,14 @@ void ShowPath_ref()
         current_time = ros::Time::now();
         //compute odometry in a typical way given the velocities of the robot
         //路径
-        if(t1 == 720)
+        if(t1 == 400)
         {
-            vy = -vy;
+            vx = -vx;
         }
 
-        double dt = 0.1;
-        double delta_x = vx * cos(th*50*PI/180) * dt;
-        double delta_y = vy * dt;
+        double dt = 0.02;
+        double delta_x = vx * dt;
+        double delta_y = vy * PI * cos(th*PI) * dt;
         double delta_th = vth * dt;
 
         x += delta_x;
@@ -124,7 +125,8 @@ void ShowPath_ref()
         this_pose_stamped.pose.position.x = x;
         this_pose_stamped.pose.position.y = y;
 
-        geometry_msgs::Quaternion goal_quat = tf::createQuaternionMsgFromYaw(th);
+        geometry_msgs::Quaternion goal_quat = tf::createQuaternionMsgFromYaw(th);//位姿，四元数与欧拉角的转化
+        
         this_pose_stamped.pose.orientation.x = goal_quat.x;
         this_pose_stamped.pose.orientation.y = goal_quat.y;
         this_pose_stamped.pose.orientation.z = goal_quat.z;
@@ -141,7 +143,7 @@ void ShowPath_ref()
 
         last_time = current_time;
         loop_rate.sleep();
-        if(t1 >= 1440)
+        if(t1 >= 800)
         {
             break;
         }
@@ -162,6 +164,7 @@ int main(int argc,char *argv[])
     //创建发布者
     send_publisher = speed_control_nh.advertise<mavros_msgs::SpeedControlSet_sub>("/mavros/speed_control/send_topic",1000);
     path_ref_pub = speed_control_nh.advertise<nav_msgs::Path>("trajectory_ref", 10, true);
+    ref_publisher = speed_control_nh.advertise<mavros_msgs::SpeedControlSet_sub>("/mavros/speed_control/ref_topic",1000);
     //订阅t265，并发布
     path_pub = speed_control_nh.advertise<nav_msgs::Path>("trajectory", 10, true);
     ros::Subscriber odomSub = speed_control_nh.subscribe<nav_msgs::Odometry>("/camera/odom/sample", 10, odomCallback);  //订阅t265里程计话题信息
@@ -170,7 +173,7 @@ int main(int argc,char *argv[])
     //初始化被发布消息
     mavros_msgs::SpeedControlSet_sub pub;
     pub.vw_set_sub= 0;
-    pub.vx_set_sub = 150;
+    pub.vx_set_sub = 0.25;
     pub.vy_set_sub = 0;
    
     //时间
@@ -179,7 +182,7 @@ int main(int argc,char *argv[])
     //初始化位置坐标为零
     for(int i = 0;i<3;i++)
     {
-        pose_cal[i] = 0; 
+        pose_ref[i] = 0; 
     }
  
     // 发布参考路径
@@ -189,42 +192,50 @@ int main(int argc,char *argv[])
     ros::Rate r(50);//两次sleep之间0.02s
     while(ros::ok())
     {
+        //时间
+        t++;
         //急刹车
-        if(safe != 0)
+        if(safe != true)
         {
             pub.vw_set_sub = 0;
             pub.vx_set_sub = 0;
             pub.vy_set_sub = 0;
             send_publisher.publish(pub);
-            ROS_INFO("publish end!");
+            ROS_INFO("emergency braking!");
             break;
         }
-        //x方向，两个周期时掉头
-        if(t % 720 == 0)
+        //前半段时间
+        if(t < 400)
+        {
+            // pub.vx_set_sub = pub.vx_set_sub + P_speed_x*(speed[0] - 0.25);
+            pose_ref[1] = 0.8 * sin(t*0.02*PI/2);//y参考坐标
+            pub.vy_set_sub = 0.2 * PI*cos(t*0.02*PI/2) 
+                            + P_pose_y * (-pose[1] - pose_ref[1]) 
+                            + P_speed_y*(-speed[1] - 0.2*PI*cos(t*0.02*PI/2));
+
+            //发布速度控制底盘
+            send_publisher.publish(pub);
+            ROS_INFO("control successfully!");
+        }
+        else if(t == 400)
         {
             pub.vx_set_sub = -pub.vx_set_sub;
         }
-        //时间
-        t++;
-        //计算理论位置
-        // pose_cal[1] += pub.vy_set_sub * 0.000413 * 0.02 ;//理论y位置(m)
-        // pose_cal[0] += pub.vx_set_sub * 0.000413 * 0.02 ;//理论x位置(m)
-        // pub.vx_set_sub =  pub.vx_set_sub 
-        //                 + P_speed_x * (pub.vx_set_sub - speed[1] * 1200)
-        //                 + P_pose_x * (pose[1] - pose_cal[0]);//x方向速度，保持恒定；根据速度位置闭环控制
-        pub.vy_set_sub = P_pose_y * (pose[2] - 300*sin(PI*t/180));//y方向速度，余弦函数;根据速度位置闭环控制
-        // pub.vy_set_sub =  cos(t*PI/180) * 300;
-        //发布速度控制底盘
-        send_publisher.publish(pub);
-        ROS_INFO("publish success!");
-        //完成任务后停车
-        if(t >= 1440 )
+        else if(t > 400 && t <800)
+        {
+            // pub.vx_set_sub = pub.vx_set_sub + P_speed_x*(speed[0] + 0.25);
+            pub.vy_set_sub = 0.2*PI*cos(t*0.02*PI/2);
+            //发布速度控制底盘
+            send_publisher.publish(pub);
+            ROS_INFO("control successfully!");
+        }
+        else if(t >= 800 )//完成任务后停车
         {
             pub.vw_set_sub = 0;
             pub.vx_set_sub = 0;
             pub.vy_set_sub = 0;
             send_publisher.publish(pub);
-            ROS_INFO("publish end!");
+            ROS_INFO("control end!");
             break;
         }
         r.sleep();
